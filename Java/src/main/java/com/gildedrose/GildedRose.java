b5b8600000e78e9a4a0626edd960af2fda1862e2
Diego Andres Torralba <diego.torralba@mercadolibre.com>
2019-10-24 17:14:40 -0300

    firstPartDojo

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class GildedRose {
     Item[] items;
 
@@ -7,56 +10,36 @@ class GildedRose {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
+    public void updateItems() {
+        List<ItemDefinition> itemDefinitions = createItemsDefinition(items);
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        for (ItemDefinition itemDefinition : itemDefinitions) {
+            itemDefinition.updateSellIn();
+            itemDefinition.updateSellIn();
+        }
+    }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+    private List<ItemDefinition> createItemsDefinition(Item[] items) {
+        List<ItemDefinition> itemDefinitions = new ArrayList<>();
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+        for(Item item : items) {
+            if (isNormal(item)) {
+                itemDefinitions.add(new NormalItem(item));
+            } else if (item.name.equals("Aged Brie")){
+                itemDefinitions.add(new AgedBrieItem(item));
+            } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                itemDefinitions.add(new LegendaryItem(item));
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                itemDefinitions.add(new BackstageItem(item));
             }
         }
+
+        return itemDefinitions;
+    }
+
+    private boolean isNormal(Item item) {
+        return !item.name.equals("Aged Brie")
+                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")
+                && !item.name.equals("Sulfuras, Hand of Ragnaros");
     }
 }
